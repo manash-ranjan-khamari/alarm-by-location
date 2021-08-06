21263d80b2669e70626ad5e2eaaa9409cb2d2a36
Manash Khamari <mkhamari@movoto.com>
2021-08-06 21:56:15 +0530

    Small refctoring done to add error message for noww

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,8 @@ var app = new Vue({
         headerMessage: 'Hello Calipsa!',
         fields: ['location', 'outcome','datetime'],
         startDateTime: '',
-        endDateTime: ''
+        endDateTime: '',
+        error: ''
     },
     components:{
         'vuetable-pagination': Vuetable.VuetablePagination
@@ -22,27 +23,34 @@ var app = new Vue({
             this.$refs.vuetable.changePage(page)
         },
         async filterData() {
-            const response = await axios.get(`/api/events/?sort=&page=1&per_page=50&startTime=${this.startDateTime}&endTime=${this.endDateTime}`);
+            const requestUrl = `/api/events/?sort=&page=1&per_page=50${this.startDateTime ? `&startTime=${this.startDateTime}`: ''}${this.startDateTime ? `&endTime=${this.endDateTime}`: ''}`;
+            try {
+                const response = await axios.get(requestUrl);
 
-            if (response.status === 200) {
-                Vue.nextTick( () => {
-                    const paginationObj = {
-                        total: response?.data?.total,
-                        per_page: response?.data?.per_page,
-                        current_page: 1,
-                        last_page: response?.data?.last_page,
-                        next_page_url: response?.data?.next_page_url,
-                        prev_page_url: response?.data?.prev_page_url,
-                        from: response?.data?.from,
-                        to: response?.data?.to
-                    };
+                if (response.status === 200) {
+                    Vue.nextTick( () => {
+                        const paginationObj = {
+                            total: response?.data?.total,
+                            per_page: response?.data?.per_page,
+                            current_page: 1,
+                            last_page: response?.data?.last_page,
+                            next_page_url: response?.data?.next_page_url,
+                            prev_page_url: response?.data?.prev_page_url,
+                            from: response?.data?.from,
+                            to: response?.data?.to
+                        };
 
-                    this.$refs.vuetable.resetData();
-                    this.$refs.vuetable.setData(response?.data?.data);
-                    this.$refs.vuetable.tablePagination = paginationObj;
-                    this.$refs.pagination.setPaginationData(paginationObj)
-                    this.$refs.vuetable.refresh();
-                })
+                        this.$refs.vuetable.resetData();
+                        this.$refs.vuetable.setData(response?.data?.data);
+                        this.$refs.vuetable.tablePagination = paginationObj;
+                        this.$refs.pagination.setPaginationData(paginationObj)
+                        this.$refs.vuetable.refresh();
+                    })
+                } else {
+                    this.error = 'Invalid Input';
+                }
+            } catch (err) {
+                this.error = 'Invalid Input';
             }
         },
     }
